a7114811e99ee845aa32b5f6e0706195a5a4435d
Léo Peyronnet <leo.peyronnet@outlook.fr>
2021-06-19 14:00:09 +0200

    Version 1.4.0.2106

--- Passliss/Classes/Global.cs
+++ Passliss/Classes/Global.cs
@@ -98,7 +98,7 @@ namespace Passliss.Classes
 		/// <summary>
 		/// The current version of Passliss.
 		/// </summary>
-		public static string Version => "1.4.0.2106-rc1";
+		public static string Version => "1.4.0.2106";
 
 		/// <summary>
 		/// GitHub link for the last version (<see cref="string"/>).
