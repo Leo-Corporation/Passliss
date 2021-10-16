941ee9e24a055710656958855889ef14871e7963
Léo Peyronnet <leo.peyronnet@outlook.fr>
2021-10-16 09:49:41 +0200

    Code cleanup

--- Passliss/Classes/Settings.cs
+++ Passliss/Classes/Settings.cs
@@ -24,12 +24,8 @@ SOFTWARE.
 using LeoCorpLibrary;
 using Passliss.Enums;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Xml.Serialization;
 
