1bc0e8365242697a0873b1f7596a4322b2d55521
Léo Peyronnet <leo.peyronnet@outlook.fr>
2021-12-06 15:37:07 +0100

    Code cleanup

--- Passliss/Pages/EncryptPage.xaml.cs
+++ Passliss/Pages/EncryptPage.xaml.cs
@@ -23,20 +23,10 @@ SOFTWARE.
 */
 using LeoCorpLibrary;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Passliss.Pages
 {
