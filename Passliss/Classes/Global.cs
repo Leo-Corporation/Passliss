5b6729f34eee784b3700b783c3f1911459d97a78
Léo Peyronnet <leo.peyronnet@outlook.fr>
2022-01-22 10:19:33 +0100

    Added Simplified Chinese translation (#120)
    Co-Authored-By: 大眼仔旭 <86864914+wcxu21@users.noreply.github.com>

--- Passliss/Classes/Global.cs
+++ Passliss/Classes/Global.cs
@@ -95,12 +95,12 @@ namespace Passliss.Classes
 		/// <summary>
 		/// List of the available languages.
 		/// </summary>
-		public static List<string> LanguageList => new() { "English (United States)", "Français (France)" };
+		public static List<string> LanguageList => new() { "English (United States)", "Français (France)", "中文（简体）" };
 
 		/// <summary>
 		/// List of the available languages codes.
 		/// </summary>
-		public static List<string> LanguageCodeList => new() { "en-US", "fr-FR" };
+		public static List<string> LanguageCodeList => new() { "en-US", "fr-FR", "zh-CN" };
 
 		/// <summary>
 		/// The current version of Passliss.
@@ -351,6 +351,10 @@ namespace Passliss.Classes
 
 				case "fr-FR": // French (FR)
 					Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("fr-FR"); // Change
+					break;
+
+				case "zh-CN": // Chinese (CN)
+					Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("zh-CN"); // Change
 					break;
 				default: // No language
 					break;
