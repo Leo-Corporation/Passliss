4a7f4ef34f45333c6d01033dc50bc9989190ee1e
Léo Peyronnet <leo.peyronnet@outlook.fr>
2022-03-12 09:47:18 +0100

    Upgraded LeoCorpLibrary to version 4.4

--- Passliss/Classes/Settings.cs
+++ Passliss/Classes/Settings.cs
@@ -22,6 +22,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 using LeoCorpLibrary;
+using LeoCorpLibrary.Enums;
 using Passliss.Enums;
 using System;
 using System.Diagnostics;
