a825544eb1c90df4da837a46568600e91dd0338a
Léo Peyronnet <leo.peyronnet@outlook.fr>
2022-06-13 13:27:44 +0200

    Version 2.5.0.2206

--- Passliss/Classes/Global.cs
+++ Passliss/Classes/Global.cs
@@ -113,7 +113,7 @@ public static class Global
 	/// <summary>
 	/// The current version of Passliss.
 	/// </summary>
-	public static string Version => "2.5.0.2206-rc1";
+	public static string Version => "2.5.0.2206";
 
 	/// <summary>
 	/// GitHub link for the last version (<see cref="string"/>).
