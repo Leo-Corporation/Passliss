3ca744d1fc95017ceb90f49f184839e1ce55c389
Léo Peyronnet <leo.peyronnet@outlook.fr>
2023-03-17 19:22:28 +0100

    Added site header section

--- Passliss Web/passliss/components/page.tsx
+++ Passliss Web/passliss/components/page.tsx
@@ -8,74 +8,78 @@ import { Home24Filled, Settings24Filled, History24Filled, LockClosed24Regular, S
 export function PageContent({ children }: LayoutProps) {
     const { t } = useTranslation("common") // default namespace (optional)
     return (
+        <>
+            <header className='p-10'>
+                <h1 className='font-bold text-5xl'>{t("title")}</h1>
+                <p>{t("hello")}</p>
+            </header>
+            <section className="grid grid-cols-4 xl:grid-cols-5">
+                <nav className='hidden md:block'>
+                    <h3 className="font-bold m-2">{t("pinned")}</h3>
+                    <Link className={buttonVariants({
+                        size: "sm",
+                        variant: "nav",
+                        className: " text-slate-700 dark:text-slate-400 h-auto pr-3 pl-0 w-full m-2",
+                    })} href={"/home"}>
+                        <div className="p-2 rounded-lg bg-gradient-to-br from-[#0088FF] to-[#2153E0]">
+                            <Home24Filled className="text-white" />
+                        </div>
+                        <p className="ml-2">{t("home")}</p>
+                    </Link>
 
-        <section className="grid grid-cols-4 xl:grid-cols-5">
-            <nav>
-                <h3 className="font-bold m-2">{t("pinned")}</h3>
-                <Link className={buttonVariants({
-                    size: "sm",
-                    variant: "nav",
-                    className: " text-slate-700 dark:text-slate-400 h-auto pr-3 pl-0 w-full m-2",
-                })} href={"/home"}>
-                    <div className="p-2 rounded-lg bg-gradient-to-br from-[#0088FF] to-[#2153E0]">
-                        <Home24Filled className="text-white" />
-                    </div>
-                    <p className="ml-2">{t("home")}</p>
-                </Link>
+                    <Link className={buttonVariants({
+                        size: "sm",
+                        variant: "nav",
+                        className: " text-slate-700 dark:text-slate-400 h-auto pr-3 pl-0 w-full m-2",
+                    })} href={"/activity"}>
+                        <div className="p-2 rounded-lg bg-gradient-to-br from-[#6F00FF] to-[#9A21E0]">
+                            <History24Filled className="text-white" />
+                        </div>
+                        <p className="ml-2">{t("activity")}</p>
+                    </Link>
 
-                <Link className={buttonVariants({
-                    size: "sm",
-                    variant: "nav",
-                    className: " text-slate-700 dark:text-slate-400 h-auto pr-3 pl-0 w-full m-2",
-                })} href={"/activity"}>
-                    <div className="p-2 rounded-lg bg-gradient-to-br from-[#6F00FF] to-[#9A21E0]">
-                        <History24Filled className="text-white" />
-                    </div>
-                    <p className="ml-2">{t("activity")}</p>
-                </Link>
+                    <Link className={buttonVariants({
+                        size: "sm",
+                        variant: "nav",
+                        className: " text-slate-700 dark:text-slate-400 h-auto pr-3 pl-0 w-full m-2",
+                    })} href={"/settings"}>
+                        <div className="p-2 rounded-lg bg-gradient-to-br from-[#B1B1B1] to-[#343C51]">
+                            <Settings24Filled className="text-white" />
+                        </div>
+                        <p className="ml-2">{t("settings")}</p>
+                    </Link>
+                    <h3 className="font-bold m-2">{t("tools")}</h3>
+                    <Link className={buttonVariants({
+                        size: "sm",
+                        variant: "nav",
+                        className: " text-slate-700 dark:text-slate-400 px-3 w-full mx-2 my-1",
+                    })} href={"/generate"}>
+                        <LockClosed24Regular className="text-white" />
 
-                <Link className={buttonVariants({
-                    size: "sm",
-                    variant: "nav",
-                    className: " text-slate-700 dark:text-slate-400 h-auto pr-3 pl-0 w-full m-2",
-                })} href={"/settings"}>
-                    <div className="p-2 rounded-lg bg-gradient-to-br from-[#B1B1B1] to-[#343C51]">
-                        <Settings24Filled className="text-white" />
-                    </div>
-                    <p className="ml-2">{t("settings")}</p>
-                </Link>
-                <h3 className="font-bold m-2">{t("tools")}</h3>
-                <Link className={buttonVariants({
-                    size: "sm",
-                    variant: "nav",
-                    className: " text-slate-700 dark:text-slate-400 px-3 w-full mx-2 my-1",
-                })} href={"/generate"}>
-                    <LockClosed24Regular className="text-white" />
+                        <p className="ml-2">{t("generate")}</p>
+                    </Link>
+                    <Link className={buttonVariants({
+                        size: "sm",
+                        variant: "nav",
+                        className: " text-slate-700 dark:text-slate-400 px-3 w-full mx-2 my-1",
+                    })} href={"/strength"}>
+                        <Shield24Regular
+                            className="text-white" />
 
-                    <p className="ml-2">{t("generate")}</p>
-                </Link>
-                <Link className={buttonVariants({
-                    size: "sm",
-                    variant: "nav",
-                    className: " text-slate-700 dark:text-slate-400 px-3 w-full mx-2 my-1",
-                })} href={"/strength"}>
-                    <Shield24Regular
-                        className="text-white" />
+                        <p className="ml-2">{t("strength")}</p>
+                    </Link>
+                    <Link className={buttonVariants({
+                        size: "sm",
+                        variant: "nav",
+                        className: " text-slate-700 dark:text-slate-400 px-3 w-full mx-2 my-1",
+                    })} href={"/encryption"}>
+                        <Translate24Regular className="text-white" />
 
-                    <p className="ml-2">{t("strength")}</p>
-                </Link>
-                <Link className={buttonVariants({
-                    size: "sm",
-                    variant: "nav",
-                    className: " text-slate-700 dark:text-slate-400 px-3 w-full mx-2 my-1",
-                })} href={"/encryption"}>
-                    <Translate24Regular className="text-white" />
-
-                    <p className="ml-2">{t("encryption")}</p>
-                </Link>
-            </nav>
-            <div className="col-start-1">{children}</div>
-        </section>
+                        <p className="ml-2">{t("encryption")}</p>
+                    </Link>
+                </nav>
+                <div className="col-start-1">{children}</div>
+            </section>
+        </>
     )
-
 }
