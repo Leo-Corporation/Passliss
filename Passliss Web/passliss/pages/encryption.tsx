96ea24808236e1b2cc5e7cc8b03d4e824aad8fc4
Léo Peyronnet <leo.peyronnet@outlook.fr>
2023-03-19 12:15:18 +0100

    Added Encryption page (#233)

--- Passliss Web/passliss/pages/encryption.tsx
+++ Passliss Web/passliss/pages/encryption.tsx
@@ -4,10 +4,64 @@ import { Layout } from "@/components/layout"
 import { PageContent } from "@/components/page"
 import useTranslation from 'next-translate/useTranslation'
 import { Translate20Regular } from "@fluentui/react-icons"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { Button } from "@/components/ui/button"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
+import { Input } from "@/components/ui/input"
+import { Textarea } from "@/components/ui/textarea"
+var CryptoJS = require("crypto-js");
 
 
 export default function EncryptionPage() {
     const { t } = useTranslation("common") // default namespace (optional)
+    let algo = "aes";
+
+    function SelectChanged(val) {
+        algo = val;
+    }
+
+    function Encrypt() {
+        let text = (document.getElementById("ToEncrypt") as HTMLInputElement).value;
+        let key = (document.getElementById("EncryptKeyInput") as HTMLTextAreaElement).value;
+        let result = document.getElementById("Encrypted") as HTMLTextAreaElement;
+
+        switch (algo) {
+            case "aes":
+                result.value = CryptoJS.AES.encrypt(text, key);
+                break;
+            case "3des":
+                result.value = CryptoJS.TripleDES.encrypt(text, key);
+
+            default:
+                break;
+        }
+    }
+
+    function Decrypt() {
+        let text = (document.getElementById("ToDecrypt") as HTMLInputElement).value;
+        let key = (document.getElementById("DecryptKeyInput") as HTMLTextAreaElement).value;
+        let result = document.getElementById("Decrypted") as HTMLTextAreaElement;
+
+        switch (algo) {
+            case "aes":
+                result.value = hex2a(CryptoJS.AES.decrypt(text, key).toString());
+                break;
+            case "3des":
+                result.value = hex2a(CryptoJS.TripleDES.decrypt(text, key).toString());
+
+            default:
+                break;
+        }
+    }
+
+    function hex2a(hex: string): string {
+        let str = "";
+        for (let i = 0; i < hex.length; i += 2) {
+            str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+        }
+        return str;
+    }
+
     return (
         <Layout>
             <Head>
@@ -25,6 +79,55 @@ export default function EncryptionPage() {
 
                     <p className="font-bold ml-2">{t("encryption")}</p>
                 </div>
+                <Tabs defaultValue="encrypt">
+                    <TabsList>
+                        <TabsTrigger value="encrypt">{t("encrypt")}</TabsTrigger>
+                        <TabsTrigger value="decrypt">{t("decrypt")}</TabsTrigger>
+                        <Select defaultValue="aes" onValueChange={SelectChanged}>
+                            <SelectTrigger className="h-auto">
+                                <SelectValue placeholder={t("algorithm")} />
+                            </SelectTrigger>
+                            <SelectContent>
+                                <SelectItem defaultChecked={true} value="aes">AES</SelectItem>
+                                <SelectItem value="3des">3DES</SelectItem>
+                            </SelectContent>
+                        </Select>
+                    </TabsList>
+                    <TabsContent className="border-none data-[state=active]:flex justify-center" value="encrypt">
+                        <div className="w-full space-y-2">
+                            <div className="flex items-center space-x-3">
+                                <label htmlFor="EncryptKeyInput">{t("key")}</label>
+                                <Input id="EncryptKeyInput" />
+                                <Button id="EncryptBtn" onClick={Encrypt}>{t("encrypt")}</Button>
+                            </div>
+                            <div className="space-y-2">
+                                <label htmlFor="ToEncrypt">{t("text-to-encrypt")}</label>
+                                <Textarea id="ToEncrypt" />
+                            </div>
+                            <div className="space-y-2">
+                                <label htmlFor="Encrypted">{t("encrypted-text")}</label>
+                                <Textarea id="Encrypted" />
+                            </div>
+                        </div>
+                    </TabsContent>
+                    <TabsContent className="border-none" value="decrypt">
+                        <div className="w-full space-y-2">
+                            <div className="flex items-center space-x-3">
+                                <label htmlFor="DecryptKeyInput">{t("key")}</label>
+                                <Input id="DecryptKeyInput" />
+                                <Button id="DecryptBtn" onClick={Decrypt}>{t("decrypt")}</Button>
+                            </div>
+                            <div className="space-y-2">
+                                <label htmlFor="ToDecrypt">{t("text-to-decrypt")}</label>
+                                <Textarea id="ToDecrypt" />
+                            </div>
+                            <div className="space-y-2">
+                                <label htmlFor="Decrypted">{t("decrypted-text")}</label>
+                                <Textarea id="Decrypted" />
+                            </div>
+                        </div>
+                    </TabsContent>
+                </Tabs>
             </PageContent>
         </Layout>
     )
