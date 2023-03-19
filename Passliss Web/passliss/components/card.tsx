26da6a42500443aa4b5e10d9fec172a14cc89682
Léo Peyronnet <leo.peyronnet@outlook.fr>
2023-03-19 14:41:49 +0100

    Improved card design

--- Passliss Web/passliss/components/card.tsx
+++ Passliss Web/passliss/components/card.tsx
@@ -8,7 +8,7 @@ export interface CardProps {
 
 export default function DashboardCard(props: CardProps) {
     return (
-        <a href={props.link} className="m-3 p-5 rounded-lg bg-white dark:bg-slate-800 text-black dark:text-white flex space-x-2 w-[350px] items-center">
+        <a href={props.link} className="m-3 p-5 rounded-lg bg-white dark:bg-slate-800 shadow-md text-black dark:text-white flex space-x-2 w-[350px] items-center hover:-translate-y-1 hover:shadow-lg transition-all">
             <p className="icon-f text-4xl text-[#0088FF]">{props.icon}</p>
             <div>
                 <h3 className="text-xl font-bold">{props.title}</h3>
