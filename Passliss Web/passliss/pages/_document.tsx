8eb9fc82afdd181de8437fbb2dc1fe22b95d89f7
Léo Peyronnet <leo.peyronnet@outlook.fr>
2023-04-01 12:13:05 +0200

    Added social meta tags

--- Passliss Web/passliss/pages/_document.tsx
+++ Passliss Web/passliss/pages/_document.tsx
@@ -16,6 +16,24 @@ export default function Document() {
           media="(prefers-color-scheme: dark)"
         />
         <link rel="apple-touch-icon" href="/images/icon-512x512.png"></link>
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image" content="/images/social.png" />
+        <meta
+          name="twitter:description"
+          content="Passliss is a web application that allows you to generate secure passwords, test the strength of exiting ones and more."
+        />
+        <meta name="twitter:title" content="Léo Corporation" />
+        <meta
+          property="og:description"
+          content="Passliss is a web application that allows you to generate secure passwords, test the strength of exiting ones and more."
+        />
+        <meta property="og:image" content="/images/social.png" />
+        <meta property="og:title" content="Léo Corporation" />
+
+        <meta
+          name="description"
+          content="Passliss is a web application that allows you to generate secure passwords, test the strength of exiting ones and more."
+        />
       </Head>
       <body className="min-h-screen font-sans text-slate-900 antialiased dark:text-slate-50">
         <Main />
--- Passliss Web/passliss/pages/activity.tsx
+++ Passliss Web/passliss/pages/activity.tsx
@@ -38,10 +38,7 @@ export default function ActivityPage() {
     <Layout>
       <Head>
         <title>Passliss</title>
-        <meta
-          name="description"
-          content="Passliss is a simple yet modern password generator."
-        />
+
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
