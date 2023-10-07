9d3348965f1ee53b8ab22294d588d679bb21a404
Léo Peyronnet <leo.peyronnet@outlook.fr>
2023-10-07 13:59:10 +0200

    Fixed config errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,16 @@
+import withPWA from "next-pwa"
 import nextTranslate from "next-translate-plugin"
-import withPWA from "next-pwa";
 
 const pwa = withPWA({
-  dest: 'public',
+  dest: "public",
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-});
+  disable: process.env.NODE_ENV === "development",
+})
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    fontLoaders: [
-      {
-        loader: "@next/font/google",
-        options: { subsets: ["latin"] },
-      },
-    ],
-  },
 }
 
 export default pwa(nextTranslate(nextConfig))
