fb86f1c25f96123f6d1253f7e3a4f236e4b82b81
Léo Peyronnet <leo.peyronnet@outlook.fr>
2024-01-02 12:12:08 +0100

    Redesigned cards in Home page (#761)

--- components/card.tsx
+++ components/card.tsx
@@ -1,3 +1,5 @@
+import { ChevronRight20Regular } from "@fluentui/react-icons"
+
 export interface CardProps {
   title: string
   icon: string
@@ -9,13 +11,19 @@ export default function DashboardCard(props: CardProps) {
   return (
     <a
       href={props.link}
-      className="m-3 flex w-[350px] items-center space-x-2 rounded-lg bg-white p-5 text-black shadow-md transition-all hover:-translate-y-1 hover:shadow-lg dark:bg-slate-800 dark:text-white"
+      className="group m-3 flex w-[350px] items-center space-x-2 rounded-lg border border-[#0088FF] bg-white p-5 text-black shadow-md shadow-[#0088ff2f] transition-all hover:-translate-y-1 hover:shadow-lg hover:shadow-[#0088ff2f] dark:bg-slate-900 dark:text-white"
     >
       <p className="icon-f text-4xl text-[#0088FF]">{props.icon}</p>
       <div>
         <h3 className="text-xl font-bold">{props.title}</h3>
         <p className="text-sm">{props.description}</p>
       </div>
+      <p>
+        <ChevronRight20Regular
+          className="transition-all group-hover:-mr-2"
+          color="#0088FF"
+        />
+      </p>
     </a>
   )
 }
