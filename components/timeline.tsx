72768acf6cd2389c82de21aa8efbb6cea52e54b0
Léo Peyronnet <leo.peyronnet@outlook.fr>
2024-04-16 09:49:58 +0200

    Added the possibility to filter history (#974)

--- components/timeline.tsx
+++ components/timeline.tsx
@@ -3,6 +3,8 @@ import useTranslation from "next-translate/useTranslation"
 
 import { Activity } from "@/types/activity"
 import { GetActivity, SortActivities } from "@/lib/browser-storage"
+import { PasswordStrength } from "@/lib/password-gen"
+import { GetPasswordStrength } from "@/lib/password-strength"
 import ActivityItem from "./activity-item"
 
 export interface TimelineProps {
@@ -11,6 +13,7 @@ export interface TimelineProps {
   hide: boolean
   index: number
   refreshEvent: Function
+  filter: string
 }
 
 export default function Timeline(props: TimelineProps) {
@@ -41,19 +44,40 @@ export default function Timeline(props: TimelineProps) {
     props.refreshEvent()
   }
 
+  function matchFilter(password: string): boolean {
+    if (props.filter === "all") return true
+    let strength: PasswordStrength = GetPasswordStrength(password)
+    switch (strength) {
+      case PasswordStrength.VeryGood:
+        return props.filter === "verygood"
+      case PasswordStrength.Good:
+        return props.filter === "good"
+      case PasswordStrength.Medium:
+        return props.filter === "medium"
+      case PasswordStrength.Low:
+        return props.filter === "low"
+      default:
+        return true
+    }
+  }
+
   return (
     <section>
       <h3 className="text-xl font-bold">{title}</h3>
       <div className="border-l border-slate-400 dark:border-slate-600">
         {els.map((el, i) => (
-          <ActivityItem
-            timeline_index={props.index}
-            index={i}
-            key={i}
-            deleteEvent={deleteElement}
-            activity={el}
-            hide={props.hide}
-          />
+          <>
+            {matchFilter(el.content) && (
+              <ActivityItem
+                timeline_index={props.index}
+                index={i}
+                key={i}
+                deleteEvent={deleteElement}
+                activity={el}
+                hide={props.hide}
+              />
+            )}
+          </>
         ))}
       </div>
     </section>
