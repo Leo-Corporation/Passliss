06e7d18760e15d5a0cf5a2e73254bb1818a2bef9
Léo Peyronnet <leo.peyronnet@outlook.fr>
2025-04-13 15:23:28 +0200

    Added Password Item

---
+++ components/password-item.tsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react"
+import { Copy16Regular } from "@fluentui/react-icons"
+
+import { getPasswordStrength } from "@/lib/password"
+import { Button } from "./ui/button"
+
+export default function PasswordItem(props: { content: string }) {
+  function Copy() {
+    navigator.clipboard.writeText(props.content)
+  }
+
+  const [icon, setIcon] = useState("")
+  const [color, setColor] = useState("")
+  function LoadIcon() {
+    switch (getPasswordStrength(props.content)) {
+      case 0:
+        setIcon("\uF36E")
+        setColor("red")
+        break
+      case 1:
+        setIcon("\uF882")
+        setColor("#FF7B00")
+        break
+      case 2:
+        setIcon("\uF299")
+        setColor("#68EA00")
+        break
+      case 3:
+        setIcon("\uF6EA")
+        setColor("#00BF07")
+        break
+      default:
+        setIcon("\uF4AB")
+        setColor("#E2E8F0")
+        break
+    }
+  }
+  useEffect(() => {
+    LoadIcon()
+  }, [])
+
+  return (
+    <div className="my-2 grid h-auto min-w-[150px] grid-cols-[24px_1fr_24px] rounded-md border-0 bg-white px-2 py-1 shadow-md dark:bg-slate-800">
+      <p className="icon-f" style={{ color: color }}>
+        {icon}
+      </p>
+      <p>{props.content}</p>
+      <Button onClick={Copy} variant="outline" className="h-auto p-0">
+        <Copy16Regular />
+      </Button>
+    </div>
+  )
+}
