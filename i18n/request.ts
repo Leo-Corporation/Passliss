a47e82a3a5a31701d616952f01c83e6df6d7a442
Léo Peyronnet <leo.peyronnet@outlook.fr>
2025-04-20 11:58:28 +0200

    Added locale switcher system

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,9 +1,8 @@
+import { getUserLocale } from "@/services/locale"
 import { getRequestConfig } from "next-intl/server"
 
 export default getRequestConfig(async () => {
-  // Provide a static locale, fetch a user setting,
-  // read from `cookies()`, `headers()`, etc.
-  const locale = "en"
+  const locale = await getUserLocale()
 
   return {
     locale,
